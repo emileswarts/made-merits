010b3ebc445832396280ee73d76c6a60dff5d894
Emile <emile.swarts123@gmail.com>
2016-07-17 15:01:10 +0100

    Create some employees.

--- resources/migrations/20160717124215-add-users-table.up.sql
+++ resources/migrations/20160717124215-add-users-table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users
-(id VARCHAR(20) PRIMARY KEY,
+(id MEDIUMINT NOT NULL auto_increment,
  first_name VARCHAR(30),
  last_name VARCHAR(30),
  email VARCHAR(30),
  admin BOOLEAN,
  last_login TIME,
  is_active BOOLEAN,
- pass VARCHAR(300));
+ pass VARCHAR(300),
+ PRIMARY KEY (id)
+);
---
+++ resources/migrations/20160717131342-add-employees.up.sql
@@ -0,0 +1,4 @@
+INSERT into users (first_name, last_name, email) values
+  ('Emile', 'Swarts', 'emile@madetech.co.uk'),
+  ('Rory', 'MacDonald', 'rory@madetech.co.uk'),
+  ('Chris', 'BlackBurn', 'chris@madetech.co.uk');
