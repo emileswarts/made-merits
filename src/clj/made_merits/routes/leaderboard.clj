7936ac38d22c3db945230d2091d11f4ab0b9e9c6
Emile <emile.swarts123@gmail.com>
2016-07-18 21:01:31 +0100

    Use new service to identify the king.

--- src/clj/made_merits/routes/leaderboard.clj
+++ src/clj/made_merits/routes/leaderboard.clj
@@ -2,12 +2,17 @@
   (:require [made-merits.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
+            [made-merits.service.winner-calculator :as winner-calculator]
             [made-merits.db.core :as db]
             [clojure.java.io :as io]))
 
+(defn meritted-users
+  []
+  (winner-calculator/with-winner (db/get-meritted-users)))
+
 (defn leaderboard []
   (layout/render
-    "leaderboard.html" {:meritted_users (db/get-meritted-users)}))
+    "leaderboard.html" {:meritted_users (meritted-users)}))
 
 (defroutes leaderboard-routes
   (GET "/" [] (leaderboard)))
