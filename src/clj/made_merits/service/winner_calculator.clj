518cfbc3f753c346537c674b0722118b2ae7bab4
Emile <emile.swarts123@gmail.com>
2016-07-18 20:58:21 +0100

    public function has to go at the top apparently

--- src/clj/made_merits/service/winner_calculator.clj
+++ src/clj/made_merits/service/winner_calculator.clj
@@ -2,13 +2,6 @@
   (:require [clojure.set :as set]
             [clojure.walk :as walk]))
 
-(defn with-winner
-  [scored-users]
-  (let [users-with-status (with-status scored-users)]
-    (if (more-than-one-winner? users-with-status)
-      (mark-everyone-as-a-loser users-with-status)
-      users-with-status)))
-
 (defn- with-status
   [scored-users]
   (let [max-score (apply max (map :score scored-users))]
@@ -34,3 +27,11 @@
           (walk/keywordize-keys
             (frequencies
               (map :status users-with-status))))))
+
+(defn with-winner
+  [scored-users]
+  (let [users-with-status (with-status scored-users)]
+    (if (more-than-one-winner? users-with-status)
+      (mark-everyone-as-a-loser users-with-status)
+      users-with-status)))
+
