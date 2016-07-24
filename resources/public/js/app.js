0117ac3af28b2832caa45a21667791f1a00ad55d
Emile <emile.swarts123@gmail.com>
2016-07-24 17:49:59 +0100

    Ability to toggle all the merits

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   $('.leader-board__user').on('click', function(e) {
     $target = e.currentTarget;
 
-    $('.leader-board__user-merits').hide();
     $($target).next('.leader-board__user-merits').toggle();
   });
 });
