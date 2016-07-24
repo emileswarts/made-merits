ac299936441b8e3b9a66b744be08ac19bbbd3077
Emile <emile.swarts123@gmail.com>
2016-07-24 14:30:51 +0100

    Show / Hide merits when clicking on user name

---
+++ resources/public/js/app.js
@@ -0,0 +1,6 @@
+$('.leader-board__user').on('click', function(e) {
+  $target = e.currentTarget;
+
+  $('.leader-board__user-merits').hide();
+  $($target).next('.leader-board__user-merits').show();
+});
