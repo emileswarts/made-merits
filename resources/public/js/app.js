db6901c880ea5a292b48598408c58b1293e2259d
Emile <emile.swarts123@gmail.com>
2016-07-24 17:46:19 +0100

    Fix jquery

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,6 +1,8 @@
-$('.leader-board__user').on('click', function(e) {
-  $target = e.currentTarget;
+$( document ).ready(function() {
+  $('.leader-board__user').on('click', function(e) {
+    $target = e.currentTarget;
 
-  $('.leader-board__user-merits').hide();
-  $($target).next('.leader-board__user-merits').toggle();
-});
+    $('.leader-board__user-merits').hide();
+    $($target).next('.leader-board__user-merits').toggle();
+  });
+};
